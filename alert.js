0c5b6fdc076d8e1ccedcbe786e55402f0f0e4e93
unknown <daisysauspurchase@icloud.com>
2020-05-18 16:43:44 +0800

    modified styles and naming

--- alert.js
+++ alert.js
@@ -1,15 +1,10 @@
 Btn = document.getElementsByClassName('btn live-btn')[0];
-Btn.style.width='500px'
-Btn.style.height='400px'
 Btn.innerHTML='<img src=https://github.com/Tup0lev/Bilibili_live_nuclear_button/blob/master/Cyka_Image.png?raw=true />';
 Btn.style.color='red'
 Btn.style.width='600px'
 Btn.style.height='391px'
 Btn.style.backgroundColor='white'
-
-
 var image = document.createElement("img");
 image.src = chrome.runtime.getURL("Cyka_Image.png");
 Btn.appendChild(image);
-console.log("BLYAAAAAAAAAAAAT")
-//Btn.innerHTML='<img src=Cyka_Image.png />';
+
--- popup.js
+++ popup.js
@@ -1,9 +1,6 @@
-// var app = chrome.runtime.getBackgroundPage();
-
-function hello() {
+function alyoo() {
   chrome.tabs.executeScript({
     file: 'alert.js'
   }); 
 }
-
-document.getElementById('clickme').addEventListener('click', hello);
+document.getElementById('clickme').addEventListener('click', alyoo);
