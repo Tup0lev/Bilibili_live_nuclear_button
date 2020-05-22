285c3065b2328a1620eb29f2b2c91af0a278a158
unknown <daisysauspurchase@icloud.com>
2020-05-22 13:05:36 +0800

    improved image loading speed

--- alert.js
+++ alert.js
@@ -1,5 +1,5 @@
 Btn = document.getElementsByClassName('btn live-btn')[0];
-Btn.innerHTML='<img src=https://github.com/Tup0lev/Bilibili_live_nuclear_button/blob/master/Cyka_Image.png?raw=true />';
+Btn.innerHTML='';
 Btn.style.color='red'
 Btn.style.width='600px'
 Btn.style.height='391px'
